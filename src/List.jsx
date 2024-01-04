0028b7e6b60d736a640f535b4af6b58bdd4b27ca
Illia Teliuk <illiateliuk@gmail.com>
2024-01-04 19:55:28 +0200

    Learned useRef webhook

--- src/List.jsx
+++ src/List.jsx
@@ -1,4 +1,10 @@
-import { React, Component, useState, useEffect } from "react";
+import {
+    React,
+    // Component,
+    useState,
+    useEffect,
+    useRef,
+} from "react";
 
 // class List extends Component {
 //     state = {
@@ -42,35 +48,44 @@ import { React, Component, useState, useEffect } from "react";
 const List = () => {
     const [items, setItems] = useState([1, 2, 3]);
     const [count, setCount] = useState(0);
+    const timerRef = useRef();
 
-    const addNumber = () =>{
-      const randomNum = Math.round(Math.random() * 10);
-      setItems([...items, randomNum]);
-    }
-
-    useEffect(() => {
-        console.log('counter was updated');
-    }, [count])
+    const addNumber = () => {
+        const randomNum = Math.round(Math.random() * 10);
+        setItems(items => [...items, randomNum]);
+    };
 
     useEffect(() => {
-        console.log('items list was updated');
-    }, [items])
+        console.log("counter was updated");
+    }, [count]);
 
     useEffect(() => {
-        console.log('component was updated');
-    })
+        console.log("items list was updated");
+    }, [items]);
 
     useEffect(() => {
-        console.log('Component did mount');
+        console.log("component was updated");
+    });
+
+    useEffect(() => {
+        console.log("Component did mount");
         return () => {
-            console.log('Component did unmount');
-        }
-    }, [])
+            console.log("Component did unmount");
+        };
+    }, []);
+
+    const start = () => {
+        timerRef.current = setInterval(addNumber, 1000);
+    };
+
+    const stop = () => {
+        clearInterval(timerRef.current);
+    };
 
     return (
         <>
-        <h1>{count}</h1>
-        <button onClick={() => setCount(count + 1)}>+</button>
+            <h1>{count}</h1>
+            <button onClick={() => setCount(count + 1)}>+</button>
             <ul>
                 {items.map((item, index) => {
                     return <li key={index}>{item}</li>;
@@ -78,8 +93,10 @@ const List = () => {
             </ul>
 
             <button onClick={addNumber}>New number</button>
+            <button onClick={start}>Start</button>
+            <button onClick={stop}>Stop</button>
         </>
     );
-}
+};
 
 export default List;
